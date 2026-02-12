f4e22fe89b0797d45fa0f698b8af4b1d07ff1a67
ItsAntonio777 <luisvideostj@gmail.com>
2026-02-11 16:26:44 -0800

    Update sesion_02.py

--- sesion_02.py
+++ sesion_02.py
@@ -1,1 +1,20 @@
+# Tuplas 
+mi_tupla = (2, 4)
+print ("Mi tupla: ", mi_tupla)
+
+#Lista
+mi_lista = [ 1, 3.1416, "Luis", mi_tupla]
+print("El primer elemento de mi lista:", mi_lista[0])
+print("El cuarto elemento de mi lista:", mi_lista[3])
+print("El tercer elemento de mi lista:", mi_lista[2])
+
+# Diccionarios
+mi_diccionario = {"mi_lista": mi_lista,
+                  "Nombre": "Luis",
+                  "Pi": 3.1416,
+                  "Tel": "664-183 93333"}
+print("Llave para accesar a mi diccionario mi_lista", mi_diccionario["mi_lista"])
+print("LLave para acceder a mi diccionario Pi", mi_diccionario["Pi"])
+print("Llave para  accesar a mi diccionario Tel", mi_diccionario["Tel"])
+
 
