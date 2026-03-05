ea007cce8c36b7f43cad937d9a81f87125e479f2
ItsAntonio777 <luisvideostj@gmail.com>
2026-03-05 11:57:29 -0800

    codigo act

--- Ogro.py
+++ Ogro.py
@@ -1,8 +1,10 @@
-from Enemigo import *
+from Enemigo import * import random
 
 class Ogro(Enemigo):
-    def __init__(self, puntos_energia=20, ataque=3):
-        super().__init__(tipo_enemigo='Ogro', puntos_energia=puntos_energia, ataque=ataque)
 
-        def habla(self):
-            print("Ogro aplastar todo")
+def 		init	(self, puntos_energia=20, ataque=3): super().		init	(tipo_enemigo='Ogro',
+puntos_energia=puntos_energia, ataque=ataque)
+
+
+def habla(self):
+print("Ogro aplastar todo!!!")
